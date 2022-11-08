8c459602e674cbd0e56692578cf7add3b0fcb5d5
shmily-2010 <112896213+shmily-2010@users.noreply.github.com>
2022-11-09 00:32:11 +0700

    Update Q1.c

--- Q1/Q1.c
+++ Q1/Q1.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 
 int main()
-{	int k,n;
+{	
+	int k,n;
+	int i;
 	double a=1,b=1,c=1,result;
 	FILE* f = fopen ("TO HOP.INP", "ab");
-    int i;
-    scanf ("%d %d",&n,&k);
-    fscanf(f,"%d %d",&n,&k);
-    fprintf (f,"%d %d\n",n,k);
+    	scanf ("%d %d",&n,&k);
+    	fscanf(f,"%d %d",&n,&k);
+    	fprintf (f,"%d %d\n",n,k);
 	for(i=1;i<=n;i++) a=a*i;
 	for(i=1;i<=k;i++) b=b*i;
 	for(i=1;i<=n-k;i++)  c=c*i;
 	result=a/(b*c);
 	FILE* fp1 = fopen("TO HOP.OUT","ab");
 	printf("%0.f\n",result);
-    fprintf(fp1,"\n%0.f\n",result);
-    fclose(f);
-    fclose(fp1);
+    	fprintf(fp1,"\n%0.f\n",result);
+    	fclose(f);
+    	fclose(fp1);
 }
 
